1d35aad801780a79376782e07761defcb372c67a
eexpress <eexpress@163.com>
2022-02-15 18:05:18 +0800

    4

--- extension.js
+++ extension.js
@@ -11,7 +11,6 @@ const ByteArray = imports.byteArray;
 
 let list = [];	//name,url
 let favor = [];	//name,url
-let urllist = '';
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
@@ -25,14 +24,14 @@ class Indicator extends PanelMenu.Button {
 			if(ok){	favor = ByteArray.toString(content).split('\n'); }
 		}
 		const tempfile = "/tmp/m3u8.all";
-	try{
-		if(!GLib.file_test(tempfile, GLib.FileTest.IS_REGULAR)){
-			lg("cat ${m3u8path}/*.m3u* > ${tempfile}");
-			GLib.spawn_command_line_sync("cat ${m3u8path}/*.m3u* > ${tempfile}");
-		}
-		urllist = ByteArray.toString(GLib.file_get_contents(tempfile)[1]);
-	}catch(e){lg("Read URLs fail.");}
-		//~ lg("1:"+urllist);
+		let urllist = '';
+		try{
+			if(!GLib.file_test(tempfile, GLib.FileTest.IS_REGULAR)){
+				GLib.spawn_command_line_sync(`cat ${m3u8path}*.m3u* > ${tempfile}`);
+			}
+			urllist = ByteArray.toString(GLib.file_get_contents(tempfile)[1]);
+		}catch(e){lg("Read URLs fail.");}
+
 		if(!urllist){
 			urllist = `
 #EXTINF:-1 ,追剧少女
@@ -51,14 +50,13 @@ http://39.134.18.65/dbiptv.sn.chinamobile.com/PLTV/88888888/224/3221226462/1.m3u
 http://39.134.18.66/dbiptv.sn.chinamobile.com/PLTV/88888888/224/3221226465/1.m3u8
 #EXTINF:-1 ,玄幻影院
 https://tx.liveplay.live.kugou.com/live/fx_hifi_1308614369_avc.m3u8
-
 `;
 		}
 
-		//~ lg("2:"+urllist);
 		const re = /#EXTINF:.*?,(.+?)\r*\n((?:http|https|rtmp):\/\/.+?)\r*\n/mg;
 		list = urllist.replace(re, "$1,$2\n").split('\n');
 		lg("3:"+list);
+		//~ lg(`cat ${m3u8path}*.m3u* > ${tempfile}`);
 
 		super._init(0.0, _('M3U8 Play'));
 
@@ -69,7 +67,7 @@ https://tx.liveplay.live.kugou.com/live/fx_hifi_1308614369_avc.m3u8
                 reactive: false, can_focus: false });
 		const input = new St.Entry({
 			name: 'searchEntry',
-			secondary_icon: new St.Icon({ icon_name: 'folder-saved-search-symbolic' }),
+			secondary_icon: new St.Icon({ icon_name: 'folder-saved-search-symbolic', icon_size: 24 }),
 			can_focus: true,
 			hint_text: _('输入文字，搜索流媒体。'),
 			x_expand: true,
@@ -78,25 +76,77 @@ https://tx.liveplay.live.kugou.com/live/fx_hifi_1308614369_avc.m3u8
 		input.clutter_text.connect('activate', (actor) => {find_add_menu();});
 		item_input.add(input);
 		this.menu.addMenuItem(item_input);
+		add_favor();
 		this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-		function find_add_menu (){
-			let s = input.text;
-			if(!s) return;
-			that.menu._getMenuItems().forEach((j)=>{if(j.url) j.destroy();});
-			list.forEach(function(str, index, array){
-				if(!str) return;
-				const [name, add] = str.split(',');
-				if(!name.includes(s)) return;
-				const item = new PopupMenu.PopupImageMenuItem("  "+name, stock_icon.icon_name);
-				item.url = add;
-				item.connect('activate', (actor) => {
-					//~ lg(actor.url);
-					GLib.spawn_command_line_async('ffplay '+actor.url);
-				});
-				that.menu.addMenuItem(item);
+		function add_favor(){
+			favor.forEach((str,i,arr) => {
+				if(str.includes(',')){
+					const [name, url] = str.split(',');
+					add_menu(name, url, true);
+				}
 			});
 		}
+
+		function find_add_menu(){
+			let s = input.text;
+			if(!s) return;
+			that.menu._getMenuItems().forEach((j)=>{if(!j.favor && j.url) j.destroy();});
+			list.forEach(function(str, index, array){
+				if(!str) return;
+				const [name, url] = str.split(',');
+				if(!name.includes(s)) return;
+				add_menu(name, url, false);
+			});
+		}
+
+		function add_menu(name, url, is_favor){
+			//~ const item = new PopupMenu.PopupImageMenuItem("  "+name, stock_icon.icon_name);
+			const item = new PopupMenu.PopupBaseMenuItem();
+			const icon_find0 = new St.Icon({ icon_name: "view-app-grid-symbolic", icon_size: 24 });
+			const icon_find1 = new St.Icon({ icon_name: "value-increase-symbolic", icon_size: 24 });
+			const icon_favor0 = new St.Icon({ icon_name: "star-new-symbolic", icon_size: 24 });
+			const icon_favor1 = new St.Icon({ icon_name: "value-decrease-symbolic", icon_size: 24 });
+			//~ const icon1 = new St.Icon({ gicon: local_gicon("heart-filled-symbolic.svg"), icon_size: 24 });
+			//~ const icon2 = new St.Icon({ gicon: local_gicon("remove-heart-symbolic.svg"), icon_size: 24 });
+			const butt = new St.Button({ child: (is_favor ? icon_favor0 : icon_find0), track_hover: true });
+			const lbl = new St.Label();
+			lbl.text = "  "+name;
+			const hbox = new St.BoxLayout();
+			hbox.add_child(butt);hbox.add_child(lbl);item.add(hbox);
+
+			item.url = url;
+			item.favor = is_favor;
+			butt.connect('enter-event', (actor) => {
+				actor.child = is_favor ? icon_favor1 : icon_find1;
+			});
+			butt.connect('leave-event', (actor) => {
+				actor.child = is_favor ? icon_favor0 : icon_find0;
+			});
+			butt.connect('clicked', (actor) => {
+				//move to favorlist, refresh menu
+				favor.push(name+","+url);
+				save_favor();
+			});
+			item.connect('activate', (actor) => {
+				GLib.spawn_command_line_async('ffplay '+actor.url);
+			});
+			that.menu.addMenuItem(item);
+		};
+
+		function save_favor(){
+			let out = '';
+			favor.forEach((str) => {
+				if(str.includes(',')){ out += str+'\n'; }
+			});
+			GLib.file_set_contents(favorfile,out);
+		}
+
+		function local_gicon(str){
+			return Gio.icon_new_for_string(
+			ExtensionUtils.getCurrentExtension().path+"/img/"+str);
+		}
+
 	}
 });
 
